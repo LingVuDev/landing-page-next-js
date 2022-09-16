702b148072471a2427fc3745b10aa7e384c58d4a
Ling Vu <m.vu@thalia.de>
2022-09-16 14:26:01 +0200

    Dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -11,9 +12,14 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Ling 👋. Passioned about web development. Mainly working on the frontend professionaly with Angular and React, but in my free time I broaden my knowledge on mobile and backend development as well.</p>
         <p>
-          Want to learn more about me? <a href="https://www.linkedin.com/in/lingvu/">LinkedIn</a>
+          Hello, I'm Ling 👋. Passioned about web development. Mainly working on
+          the frontend professionaly with Angular and React, but in my free time
+          I broaden my knowledge on mobile and backend development as well.
+        </p>
+        <p>
+          Want to learn more about me?{" "}
+          <a href="https://www.linkedin.com/in/lingvu/">LinkedIn</a>
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -21,11 +27,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
