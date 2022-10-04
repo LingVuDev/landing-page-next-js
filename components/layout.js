d46ad73e4c00c37db7e961587d68c5260ade3812
Ling Vu <m.vu@thalia.de>
2022-10-04 14:41:13 +0200

    Clean header

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
+export const siteTitle = "Ling Vu's personal website";
+
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
@@ -11,16 +13,8 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Ling Vu's personal website"
+          content={siteTitle}
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
